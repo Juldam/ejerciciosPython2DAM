e1f1d595669df908b0154be4b4f6d999bf4d487c
Julián Pelayo <jvpelayor@salesianospizarrales.com>
2020-01-20 12:09:33 +0100

    Ejercicio 5 acabado

--- Clases/ejc5.py
+++ Clases/ejc5.py
@@ -18,6 +18,15 @@ class Contacto(object):
         self.email=email
 
 
+def comprobarNombre(nom):
+    enc=False
+    for i in misContactos:
+        if i.nombre==str(nom):
+            enc=True
+    return enc
+
+
+
 while(True):
     print("Escoja una opción: ")
     print("a) Añadir contacto")
@@ -28,18 +37,52 @@ while(True):
     r=str(input("Introduzca letra: "))
 
     if r=="a":
-        print("opc a")
+        while(True):
+            nom=str(input("Introduzca el nombre del contacto: "))
+            if comprobarNombre(nom) == True:
+                print("Ese nombre ya está en la lista, introduzca uno diferente")
+            else:
+                break
+        tel=input("Introduzca el teléfono del contacto: ")
+        email=str(input("Introduzca el email del contacto: "))
+        p=Contacto(nom, tel, email)
+        misContactos.append(p)
+        print("Contacto añadido a la agenda")
     elif r=="b":
-        print("opc b")
+        for element in misContactos:
+            print(element.nombre)
     elif r == "c":
-        print("opc c")
+        enc=False
+        contacto=str(input("Introduzca el contacto que quiere buscar: "))
+        for i in misContactos:
+            if contacto == str(i.nombre):
+                print(i.nombre+": "+"Telefono->"+i.telefono+", e-mail->"+i.email)
+                enc=True
+        if enc==False:
+            print("No se ha encontrado ese contacto en la agenda")
     elif r == "d":
-        print("opc d")
+        enc = False
+        contacto = str(input("Introduzca el contacto que quiere editar: "))
+        for i in misContactos:
+            if contacto == str(i.nombre):
+                print("El contacto "+i.nombre + " que quiere editar tiene los siguientes datos: " + "Telefono->" + i.telefono + ", e-mail->" + i.email+"\n")
+                n=str(input("Introduzca nuevo nombre para el contacto: "))
+                i.nombre=n
+                tel=input("Introduzca nuevo telefono para el contacto: ")
+                i.telefono=tel
+                e=str(input("Introduzca nuevo e-mail para el contacto: "))
+                i.email=e
+                print("Contacto editado")
+                enc = True
+        if enc == False:
+            print("No se ha encontrado ese contacto en la agenda")
     elif r == "e":
         print("Hasta otra!")
         break
     else:
         print("Debe introducir una letra entre la a y la e")
+
+
 
 
 
