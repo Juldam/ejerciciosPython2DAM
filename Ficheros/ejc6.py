c49246be9af126bc44a6c84e160d92e3716438fb
Julián Pelayo <jvpelayor@salesianospizarrales.com>
2020-01-30 23:32:50 +0100

    Ejercicios de ficheros acabados

--- Ficheros/ejc6.py
+++ Ficheros/ejc6.py
@@ -2,3 +2,25 @@
 Diseña un programa que cuente el número de caracteres de un fichero de texto,
 incluyendo los saltos de línea. (El nombre del fichero se pide al usuario por teclado).
 '''
+
+
+def contarCaracteres(fichero):
+    try:
+        f=open(fichero, "r")
+        contCaract = 0
+        for linea in f:
+            contCaract = contCaract + (len(linea))
+        print("Ese fichero tiene " + str(contCaract) + " carácteres incluyendo los saltos de línea.")
+    except IOError:
+        print("Ese archivo no existe. Inténtelo de nuevo.")
+    except:
+        print("Ha habido un error a la hora de leer el archivo...")
+
+
+
+while(True):
+    nomFichero=str(input("Introduzca la ruta/nombre de un fichero de texto para saber los carácteres que tiene: "))
+    contarCaracteres(nomFichero)
+    resp=str(input("¿Desea introducir otra palabra?(S/N)"))
+    if resp.upper() != "S":
+        break
